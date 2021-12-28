1dbf9e70c7b4f276964094f2700d0784edbf468a
Android-f545gfg4fc4p <Android-f545gfg4fc4p@DESKTOP-188NH2L>
2021-12-28 06:50:16 +0100

    [javaTcoding] added new java file

---
+++ TdTest/src/Agg.java
@@ -0,0 +1,11 @@
+
+public class Agg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   System.out.println("ccccc");
+   System.out.println("ccccc");
+   System.out.println("ccccc");
+	}
+
+}
